6178f53bda4a1ab4c9312796e353c4863743891d
SpencerWallace007463307 <77997705+SpencerWallace007463307@users.noreply.github.com>
2021-02-04 21:27:21 -0800

    Update api_test.js

--- Core Assignments/api_test.js
+++ Core Assignments/api_test.js
@@ -1,16 +1,16 @@
-const fetch = require('node-fetch');
 const http = require('http');
 let sunrise;
-fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today')
-    .then(res => res.json())
-    .then(json => sunrise = json);
 
+    fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today')
+        .then(res => res.json()) //parses json
+        .then(data => {console.log(data.results.astronomical_twilight_end);
+        sunrise = data});
 
-function onRequest(request, response){
-    response.writeHead(500, {'Content-Type': 'text/html'});
-    response.write(JSON.stringify(sunrise));
-    console.log(JSON.stringify(sunrise));
-    response.end();
-}
+    function onRequest(request, response){
+        response.writeHead(500, {'Content-Type': 'text/html'});
+        response.write(JSON.stringify(sunrise));
+        response.end();
+    }
 
-http.createServer(onRequest).listen(8888);
+    http.createServer(onRequest).listen(8888);
+
